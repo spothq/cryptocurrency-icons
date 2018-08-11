2272918f9bbbd97aaf51362c09ecaad98d210520
pierreneter <pierreneter@gmail.com>
2018-08-11 13:33:21 +0700

    update script to build manifest

--- scripts/manifest.js
+++ scripts/manifest.js
@@ -1,16 +1,15 @@
 const manifest = require('../manifest.json');
-const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const coins = require('coinlist');
 const alphaSort = require('alpha-sort');
 
-const icons = manifest.icons.map(icon => {
+const icons = manifest.map(icon => {
 	const id = typeof icon === 'string' ? icon : icon.symbol;
 
 	return {
 		name: coins.get(id, 'name') || id,
-		symbol: id,
+		symbol: id.toUpperCase(),
 	};
 });
 
@@ -18,4 +17,4 @@ icons.sort((a, b) => {
 	return alphaSort.asc(a.symbol, b.symbol);
 });
 
-fs.writeFileSync(path.resolve(__dirname, '../manifest.json'), JSON.stringify({ icons }, null, 4));
+fs.writeFileSync(path.resolve(__dirname, '../manifest.json'), JSON.stringify(icons, null, 4));

5cddd4dbba44e9f1c8340c70b8201bd0260c9911
Gabriel Cardona <gabriel@bitcoin.com>
2019-03-04 12:37:45 +0800

    Porting over to BCH.

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-# Cryptocurrency Icons
+# Bitcoin Cash Token Icons
 
 [This project](http://cryptoicons.co) contains over 6000 icons – almost 400 crypto currencies, in 4 styles and a range of sizes. Completely free. All we ask is that you don’t claim them as your own, and share this resource with others.
 
-We also have some fiat currencies: `USD`, `GBP`, `EUR`, `JPY`, `CNY`, `RUB`. *(We don't intend to add more)*
+We also have some fiat currencies: `USD`, `GBP`, `EUR`, `JPY`, `CNY`, `RUB`. _(We don't intend to add more)_
 
 Images are in the PNG format (transparent) in `32×32`, `32×32 (2x)`, and `128×128` variants, along with SVG versions.
 
 There's also a [`generic` icon](https://github.com/atomiclabs/cryptocurrency-icons/blob/master/svg/color/generic.svg) that can be used for cryptocurrencies missing an icon here.
 
 If there is an altcoin you’re looking for that hasn’t been included, please [create a new issue](https://github.com/atomiclabs/cryptocurrency-icons/issues/new).<br><sub>(We don't accept pull request contributions for new or updated icons at this time. Open an issue and we'll do the icon for you.)</sub>
-
 
 ## Install 🚀
 
@@ -20,7 +19,6 @@ $ npm install cryptocurrency-icons
 You can then import the icons from `./node_modules/cryptocurrency-icons`, for example `./node_modules/cryptocurrency-icons/svg/color/kmd.svg`.
 
 There's also a `manifest.json` file included in the package with a list of the currency symbol and name of the icons.
-
 
 ## Special Thanks 👏
 
--- scripts/manifest.js
+++ scripts/manifest.js
@@ -1,51 +1,53 @@
-'use strict';
-const fs = require('fs');
-const path = require('path');
-const coins = require('coinlist');
-const alphaSort = require('alpha-sort');
-const manifest = require('../manifest.json');
+"use strict"
+const fs = require("fs")
+const path = require("path")
+const coins = require("coinlist")
+const alphaSort = require("alpha-sort")
+const manifest = require("../manifest.json")
 
 const overrides = new Map([
-	['VRSC', 'VerusCoin'],
-	['GMR', 'Gimmer'],
-	['NEXO', 'Nexo'],
-	['GUSD', 'Gemini dollar'],
-	['CALL', 'Capital'],
-	['BOS', 'BOScoin'],
-	['CIX', 'Cryptonetix'],
-	['COQUI', 'COQUI Cash'],
-	['DEEZ', 'DeezNuts'],
-	['MZC', 'MAZA'],
-	['CVC', 'Civic'],
-	['BTM', 'Bitmark'],
-	['GLXT', 'GLX Token'],
-	['ONG', 'SoMee.Social'],
-	['CC', 'CoinCollect'],
-	['2GIVE', '2Give'],
-	['BOOTY', 'Booty'],
-	['PUNGO', 'Pungo Token'],
-	['X', 'GLX Equity Token'],
-	['AYWA', 'Aywa'],
-	['CHAIN', 'Chainmakers'],
-	['LPT', 'Livepeer Token'],
-	['AUDR', 'AUDRamp'],
-	['BAB', 'Bitcoin Cash ABC'],
-	['BSV', 'BitcoinSV'],
-	['GOLD', 'Dragonereum Gold'],
-	['USDC', 'USD Coin']
-]);
+	["VRSC", "VerusCoin"],
+	["GMR", "Gimmer"],
+	["NEXO", "Nexo"],
+	["GUSD", "Gemini dollar"],
+	["CALL", "Capital"],
+	["BOS", "BOScoin"],
+	["CIX", "Cryptonetix"],
+	["COQUI", "COQUI Cash"],
+	["DEEZ", "DeezNuts"],
+	["MZC", "MAZA"],
+	["CVC", "Civic"],
+	["BTM", "Bitmark"],
+	["GLXT", "GLX Token"],
+	["ONG", "SoMee.Social"],
+	["CC", "CoinCollect"],
+	["2GIVE", "2Give"],
+	["BOOTY", "Booty"],
+	["PUNGO", "Pungo Token"],
+	["X", "GLX Equity Token"],
+	["AYWA", "Aywa"],
+	["CHAIN", "Chainmakers"],
+	["LPT", "Livepeer Token"],
+	["AUDR", "AUDRamp"],
+	["BAB", "Bitcoin Cash ABC"],
+	["BSV", "BitcoinSV"],
+	["GOLD", "Dragonereum Gold"],
+	["USDC", "USD Coin"]
+])
 
 const icons = manifest.map(icon => {
-	const id = typeof icon === 'string' ? icon : icon.symbol;
+	console.log("ICON", icon)
+	const txid = icon.txid
+	const id = icon.symbol
 
 	return {
 		symbol: id.toUpperCase(),
-		name: overrides.get(id) || coins.get(id, 'name') || id
-	};
-});
+		name: overrides.get(id) || coins.get(id, "name") || id
+	}
+})
 
-icons.sort((a, b) => alphaSort.asc(a.symbol, b.symbol));
+icons.sort((a, b) => alphaSort.asc(a.symbol, b.symbol))
 
-const data = JSON.stringify(icons, null, '\t') + '\n';
+const data = `${JSON.stringify(icons, null, "\t")}\n`
 
-fs.writeFileSync(path.resolve(__dirname, '../manifest.json'), data);
+fs.writeFileSync(path.resolve(__dirname, "../manifest.json"), data)

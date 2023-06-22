c15d0018c95c94a4438e23b71368ac15e420b9e5
Seetharaman GR <seethagr@bitgo.com>
2023-06-22 13:09:12 +0530

    chore(icons): add icons for bct token
    Ticket: BG-79329

---
+++ react/bct.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+const SvgBct = (props) => /* @__PURE__ */ React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", xmlnsXlink: "http://www.w3.org/1999/xlink", width: "32px", height: "32px", viewBox: "0 0 32 32", ...props }, /* @__PURE__ */ React.createElement("g", { id: "surface1" }, /* @__PURE__ */ React.createElement("path", { style: {
+  stroke: "none",
+  fillRule: "nonzero",
+  fill: "rgb(20.392157%,20.392157%,20.392157%)",
+  fillOpacity: 1
+}, d: "M 15.996094 0.00390625 L 15.644531 0.730469 L 15.644531 21.886719 L 15.996094 22.101562 L 31.988281 16.296875 Z M 15.996094 0.00390625 " }), /* @__PURE__ */ React.createElement("path", { style: {
+  stroke: "none",
+  fillRule: "nonzero",
+  fill: "rgb(54.901961%,54.901961%,54.901961%)",
+  fillOpacity: 1
+}, d: "M 15.996094 0.00390625 L 0 16.296875 L 15.996094 22.101562 Z M 15.996094 0.00390625 " }), /* @__PURE__ */ React.createElement("path", { style: {
+  stroke: "none",
+  fillRule: "nonzero",
+  fill: "rgb(23.529412%,23.529412%,23.137255%)",
+  fillOpacity: 1
+}, d: "M 15.996094 23.960938 L 15.796875 24.109375 L 15.796875 31.644531 L 15.996094 31.996094 L 32 18.160156 Z M 15.996094 23.960938 " }), /* @__PURE__ */ React.createElement("path", { style: {
+  stroke: "none",
+  fillRule: "nonzero",
+  fill: "rgb(54.901961%,54.901961%,54.901961%)",
+  fillOpacity: 1
+}, d: "M 15.996094 31.996094 L 15.996094 23.960938 L 0 18.160156 Z M 15.996094 31.996094 " }), /* @__PURE__ */ React.createElement("path", { style: {
+  stroke: "none",
+  fillRule: "nonzero",
+  fill: "rgb(7.843137%,7.843137%,7.843137%)",
+  fillOpacity: 1
+}, d: "M 15.996094 22.101562 L 31.988281 16.296875 L 15.996094 11.832031 Z M 15.996094 22.101562 " }), /* @__PURE__ */ React.createElement("path", { style: {
+  stroke: "none",
+  fillRule: "nonzero",
+  fill: "rgb(22.352941%,22.352941%,22.352941%)",
+  fillOpacity: 1
+}, d: "M 0 16.296875 L 15.996094 22.101562 L 15.996094 11.832031 Z M 0 16.296875 " })));
+export default SvgBct;
